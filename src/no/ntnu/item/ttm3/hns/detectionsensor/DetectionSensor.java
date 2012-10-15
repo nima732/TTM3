d335155d137e4f66d466652e461209a16d0aa3fd
Snorre Lothar von Gohren Edwin <snorre.edwin@gmail.com>
2012-10-15 13:01:41 +0200

    started the base project

---
+++ src/no/ntnu/item/ttm3/hns/detectionsensor/DetectionSensor.java
@@ -0,0 +1,7 @@
+package no.ntnu.item.ttm3.hns.detectionsensor;
+
+import no.ntnu.item.arctis.runtime.Block;
+
+public class DetectionSensor extends Block {
+
+}
---
+++ src/no/ntnu/item/ttm3/hns/lightinggui/LightingGUI.java
@@ -0,0 +1,7 @@
+package no.ntnu.item.ttm3.hns.lightinggui;
+
+import no.ntnu.item.arctis.runtime.Block;
+
+public class LightingGUI extends Block {
+
+}
