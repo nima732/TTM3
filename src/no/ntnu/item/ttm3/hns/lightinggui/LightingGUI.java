063247742f8e4ba383ba50b0b98441c2381ab99f
Snorre Lothar von Gohren Edwin <snorre.edwin@gmail.com>
2012-10-18 15:22:55 +0200

    started working on the GUI

--- src/no/ntnu/item/ttm3/hns/lightinggui/LightingGUI.java
+++ src/no/ntnu/item/ttm3/hns/lightinggui/LightingGUI.java
@@ -1,7 +1,43 @@
 package no.ntnu.item.ttm3.hns.lightinggui;
 
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 import no.ntnu.item.arctis.runtime.Block;
+import no.ntnu.item.ttm3.hns.button.Button;
 
 public class LightingGUI extends Block {
+
+	public void createView() {
+		JFrame frame = new JFrame("Lightingapp");
+		GridLayout buttonLayout = new GridLayout(3,2);
+		GridLayout roomLayout = new GridLayout(3,3);
+		JButton start;
+		JLabel room1,room2,room3,room4,room5,room6,room7,room8,room9;
+		
+		JPanel roomPanel = new JPanel();
+		roomPanel.setLayout(roomLayout);
+		
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(buttonLayout);
+		
+		start = new JButton("Start");
+		buttonPanel.add(start);
+		
+		for (int i = 1; i < 10; i++) {
+			JLabel room = new JLabel("room"+i);
+			roomPanel.add(room);
+		}
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+		
+		
+	}
 
 }
